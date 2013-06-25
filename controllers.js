80cba3923b07c7f2bea81357cf1958d802581fef
Ricardo Rosales <missingcharacter@gmail.com>
2013-06-25 12:58:39 -0700

    Step-03

--- controllers.js
+++ controllers.js
@@ -2,3 +2,14 @@ function HelloController($scope) {
   $scope.greeting = { text: 'Hello' };
 }
 
+function CartController($scope) {
+	$scope.items = [
+		{title: 'Pulparindo', quantity: 8, price: 3.95},
+		{title: 'Soda 600 ml', quantity: 17, price: 12.95},
+		{title: 'Cacahuates', quantity: 5, price: 6.95}
+	];
+
+	$scope.remove = function(index) {
+		$scope.items.splice(index, 1);
+	}
+}
